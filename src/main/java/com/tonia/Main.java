8d4b94b2ee7b4421bfa7e117232c4fde19a10b9b
Task_1_1 <cooltone4ka@gmail.com>
2018-10-11 21:45:27 +0300

    initial commit

--- src/main/java/com/tonia/Main.java
+++ src/main/java/com/tonia/Main.java
@@ -2,6 +2,6 @@ package com.tonia;
 
 public class Main {
     public static void main(String[]args){
-        System.out.println("Тоня Лобода,Лицей№1");
+        System.out.println("Тоня Лобода,Лицей№1,11-В");
     }
 }
